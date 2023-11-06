009108052e8dadf22f87fa6c482838c25ee6267f
jeanfdp <jfduple@gmail.com>
2023-11-05 21:00:32 -0500

    Add BoidState struct and update! function

--- src/Boids.jl
+++ src/Boids.jl
@@ -1,5 +1,29 @@
 module Boids
 
-# Write your package code here.
+using LinearAlgebra: normalize
+using Distributions: MvNormal
+
+struct BoidState
+    positions::Vector{Vector{Float64}}
+    orientations::Vector{Vector{Float64}}
+end
+
+
+
+function BoidState(n::Int, box_size::Tuple)
+    dim = length(box_size)
+    positions = [rand(dim) .* box_size for _ in 1:n]
+    orientations = [normalize(rand(MvNormal(dim, 1))) for _ in 1:n]
+    BoidState(positions, orientations)
+end
+
+function update!(state::BoidState, box_size::Tuple, v::Float64, w::Float64,ruleOrientations!::Function,rulePositions!::Function)
+    dim = length(box_size)
+    n = length(state.positions)
+    for i in 1:n
+        ruleOrientations!(state, i, w)
+        rulePositions!(state, i, v, box_size)
+    end
+end
 
 end
